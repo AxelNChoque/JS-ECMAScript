277a3aa1397bd4eb12dcb1b99e20bcd462a7d6b7
Axel Choque <axel_chooque@hotmail.com>
2022-07-28 01:39:16 -0300

    Clase 8 y fin de ES8

--- es8/main.js
+++ es8/main.js
@@ -7,22 +7,50 @@ const data = {
 }
 
 const entries = Object.entries(data);
-console.log(entries);
-console.log(entries.length);
+//console.log(entries);
+//console.log(entries.length);
 
 const values = Object.values(data);
 
-console.log(values);
+//console.log(values);
 
 //padding
 
 const string = 'hello';
-console.log(string.padStart(7,'hi'));
+//console.log(string.padStart(7,'hi'));
 
-console.log(string.padEnd(12,'----------'));    
+//console.log(string.padEnd(12,'----------'));    
 
 //trueling coma
 
 const obj = {
     name: 'oscar',
 }
+
+//Async await
+
+const helloWorld = () => {
+    return new Promise ((resolve,reject) => {
+        (true)
+            ? setTimeout( () => resolve('Hello World'), 3000)
+            :reject(new Error('Test Error'))
+    })
+};
+
+const helloAsync = async () => {
+    const hello = await helloWorld();
+    console.log(hello);
+}
+
+helloAsync();
+
+const anotherFunction = async () => {
+    try {
+        const hello = await helloWorld();
+        console.log(hello);
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+anotherFunction();
