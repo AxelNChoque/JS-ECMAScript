9a32418b333c3fb460557278707bc9e4177676f8
Axel Choque <axel_chooque@hotmail.com>
2022-07-28 01:19:58 -0300

    Clase 6 y fin de ES7

---
+++ es7/main.js
@@ -0,0 +1,14 @@
+let numbers = [1,2,3,5,7,8,9,'Tato'];
+
+if (numbers.includes('Tato')){
+    console.log('Si se encuentra el valor Tato');
+} else {
+    console.log('No se encuentra');
+}
+
+
+let base = 4;
+let exponente = 3;
+let resultado = base ** exponente;
+
+console.log(resultado);
